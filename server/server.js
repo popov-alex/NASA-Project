9421a25748c9fb6d20e9989f84e8ef2a219b59a7
popov-alex <ayp5xc@virginia.edu>
2023-06-23 16:40:39 +0300

    connected SpaceX API

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 import http from 'http';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 import { mongoConnect } from './src/services/mongo.js';
 import { app } from './src/app.js';
 import { loadPlanets } from './src/models/planets.model.js';
 import { getSpaceXFlights } from './src/models/launches.model.js';
 
-dotenv.config({ path: './.env' });
+const __filename = fileURLToPath(import.meta.url || '');
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: __dirname + '/.env' });
 
 const PORT = process.env.PORT || 8000;
 
@@ -14,7 +19,7 @@ const server = http.createServer(app);
 
 (async function startServer() {
   try {
-    await mongoConnect();
+    await mongoConnect(process.env.MONGODB_URL);
     await getSpaceXFlights();
     await loadPlanets();
     server.listen(PORT, () =>
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,5 @@
 import mongoose from 'mongoose';
 
-//fix env not working
-const MONGODB_URL = 'mongodb+srv://Sasha:Sasha@nasa-api.bc2sczc.mongodb.net/';
-
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
 });
@@ -11,6 +8,6 @@ mongoose.connection.on('error', (err) => {
   console.error(err);
 });
 
-export const mongoConnect = async () => await mongoose.connect(MONGODB_URL);
+export const mongoConnect = async (URL) => await mongoose.connect(URL);
 
 export const mongoDisconnect = async () => await mongoose.disconnect();
